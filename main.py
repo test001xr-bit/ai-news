0a4b95f3ac74fa71453b1ca62c4b699735842e00
wang <xinyuewang@hotmail.com>
2026-05-07 21:47:46 +0200

    first working version

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import json
+import datetime
 
 def main():
-    print("AI News System Running...")
+    now = datetime.datetime.now()
+
+    with open("output/index.html", "w", encoding="utf-8") as f:
+        f.write(f"""
+        <html>
+        <body>
+        <h1>AI News System</h1>
+        <p>Generated at: {now}</p>
+        </body>
+        </html>
+        """)
 
 if __name__ == "__main__":
     main()
